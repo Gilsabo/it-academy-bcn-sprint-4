3f995c388f114d3d142e70a32f0eb22465364c61
Gil Sala <gilsala_88@hotmail.com>
2023-02-12 12:02:54 +0100

    Solve exercise 2

--- src/films.js
+++ src/films.js
@@ -10,13 +10,25 @@ function getAllDirectors(array) {
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
 
+const resultMoviesFromDirector = array.filter(object=> {
+  if (object.director === director) {
+    return object.title;
+  }
 
-  
+})
+
+console.log("EXERCICE 2 ->", resultMoviesFromDirector);
+return resultMoviesFromDirector;
  
 }
 
+
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
+
+const resultMoviesAverageOfDirector = 
+  console.log("EXERCICE 3 ->", resultMoviesAverageOfDirector);
+return resultMoviesAverageOfDirector;
   
 }
 
